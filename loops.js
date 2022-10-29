3f37a10d0fac69d7563fa69496563f5a50aea0aa
Bar Kaziro <barkaziro@gmail.com>
2022-10-29 15:24:30 +0300

    First Commit

---
+++ loops.js
@@ -0,0 +1,39 @@
+
+// for (let i = 0; i >= 5; i--)
+// if (i % 2 !== 0) console.log(i);
+
+// let i = 6;
+// while(i <= 12){
+// if (i % 2 !== 0) console.log(i);
+// i++;
+// }
+
+
+// do-while
+// let j = 0;
+// do {
+//   if (j % 2 !== 0) console.log(j);
+//       j++;
+// } while (j <= 5);
+
+
+//for...in
+// // Dot Notation
+// person.name
+
+// // Bracket Notation
+// person['name']
+
+// const person = {name: 'Mosh', age:30};
+
+// for (let key in person)
+// console.log(key, person[key]); 
+
+
+// for-of
+// const colors = ['red', 'green', 'blue'];
+// for(let color of colors)
+// console.log(color);
+
+let x = null;
+console.log('x');

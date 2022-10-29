689a9f10d949050f5390fc0b766348f7223627cb
Bar Kaziro <kaziromail@gmail.com>
2022-10-29 17:28:33 +0300

    Function 60% completed

--- functions.js
+++ functions.js
@@ -12,8 +12,35 @@
 // };
 
 // Arguments
-function sum(a, b){
-  return Math.max(a,b); // 1 + undefined = NaN
-}
+// function sum(a, b){
+//   return Math.max(a,b); // 1 + undefined = NaN
+// }
 
-console.log(sum(1));
+// console.log(sum(1));
+
+// function sum(){
+//   let total = 0;
+//   for (let value of arguments)
+//     total+= value;
+//   return total;
+// }
+
+// console.log(sum(1,2,3,4,5,10))
+
+// The Rest Operator
+
+// function calcDiscount(discount, ...prices){
+//   const total = prices.reduce((a, b) => a + b);
+//   return total * (1 - discount);
+// }
+
+// console.log(calcDiscount(0.1, 20, 30));
+
+// // Default Values
+// function interest(principal, rate = 3.5, years = 5){
+//   return principal * rate / 100 * years;
+// }
+
+// console.log((interest(10000)));
+
+// Getters & Setters
